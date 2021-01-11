19bed77279697408f194a573d0190550e88e2a4c
Erik Nygren <sten.erik.nygren@gmail.com>
2021-01-11 15:08:13 +0100

    more fixes

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-echo "lning to $PWD"
+echo "linking to $PWD"
 ln -sf "$PWD/dotgitconfig" ~/.gitconfig
 ln -sf "$PWD/dotjshint-config"  ~/.jshint-config
 ln -sf "$PWD/dotprofile"  ~/.profile
@@ -10,8 +10,10 @@ ln -sf "$PWD/dotvimrc" ~/.vimrc
 ln -sf "$PWD/git_prompt.sh" ~/git_prompt.sh
 VSCODE_DIR="$HOME/Library/Application Support/Code/User/"
 if [[ -d "$VSCODE_DIR" ]]; then
+  echo "linking vscode settings to $VSCODE_DIR"
   ln -sf "$PWD/vscodesettings.json" "$VSCODE_DIR/settings.json"
+  ln -sf "$PWD/vscodekeybindings.json" "$VSCODE_DIR/keybindings.json"
 else
-  echo "no vscode dir"
+  echo "$VSCODE_DIR not present, skipping linking"
 fi
 
